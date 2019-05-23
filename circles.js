94b68a832acb88ab2e5e03e0cd11ac8fc182f31b
Blaze2305 <prajpran@gmail.com>
2019-05-23 22:28:35 +0530

    added screenshot of fractal with n=100,increase=0.00001,div_fact=2;

--- circles.js
+++ circles.js
@@ -1,16 +1,19 @@
-let k=3;
+let k=-3;
+let j;
 let large;
 let children=[];
-let n=10;
-let increase=0.0005
+let n=100;
+let increase=0.00001;
+let points=[];
+let div_fact=2;
 
 function setup(){
 
   createCanvas(800,800);
-  large=new first(400,400,300,1);
+  large=new first(400,400,400,1);
   children[0]=large;
   for(let i=1;i<n;i+=1){
-    children[i]=new circles(children[i-1],i)
+    children[i]=new circles(children[i-1],i+1)
   }
 
 }
@@ -21,10 +24,21 @@ function draw(){
   background(0);
   children[0].show();
   children[0].update();
-  for(let j=1;j<n;j+=1){
+  for(j=1;j<n;j+=1){
     children[j].show();
     children[j].update(children[j-1]);
   }
+  points.push([children[j-1].px+children[j-1].x,children[j-1].y+children[j-1].py])
+
+  push();
+  noFill();
+  beginShape();
+  stroke(255);
+  for(let v=0;v<points.length;v+=1){
+    vertex(points[v][0],points[v][1]);
+  }
+  endShape();
+  pop();
 }
 
 
@@ -71,7 +85,7 @@ class first{
 class circles{
 
   constructor(parent,n){
-    this.rad=parent.rad/2;
+    this.rad=parent.rad/div_fact;
     this.time=0;
     this.n=n;
     this.speed=pow(k,n-1);
